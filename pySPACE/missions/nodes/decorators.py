74694a53b0ec1113b99f85f202e66a12ee2906b0
Torben Hansing <hansa@tzi.de>
2016-01-04 14:56:32 +0000

    Added new decorator

--- pySPACE/missions/nodes/decorators.py
+++ pySPACE/missions/nodes/decorators.py
@@ -1,6 +1,6 @@
 import abc
 import copy
-
+import warnings
 
 PARAMETER_ATTRIBUTE = "__hyperparameters"
 
@@ -26,11 +26,13 @@ class ParameterDecorator(object):
 
     def __call__(self, class_):
         if not hasattr(class_, PARAMETER_ATTRIBUTE):
-            # No __hyperparameters attribute, create a new one
-            setattr(class_, PARAMETER_ATTRIBUTE, {})
-        # Execute the Decorator
+            # No hyper parameter attribute, create a new one
+            setattr(class_, PARAMETER_ATTRIBUTE, set())
+        # Deep copy the parameter attribute to avoid side-effects to super-classes
         parameters = copy.deepcopy(getattr(class_, PARAMETER_ATTRIBUTE))
+        # Execute the Decorator on the copy
         self.execute(parameters)
+        # And replace the attribute with the copy
         setattr(class_, PARAMETER_ATTRIBUTE, parameters)
         # Return the class object
         return class_
@@ -40,8 +42,23 @@ class ParameterDecorator(object):
             return self.parameter_name == other.parameter_name
         return False
 
+    def __ne__(self, other):
+        return not self.__eq__(other)
 
-class ChoiceParameter(ParameterDecorator):
+    def __hash__(self):
+        return hash(self.parameter_name)
+
+
+class AddParameterWithWarningMixIn(ParameterDecorator):
+
+    def execute(self, parameters):
+        if self in parameters:
+            warnings.warn("Duplicate parameter definition for parameter '%s'! Replacing with the new definition" % self)
+            parameters.remove(self)
+        parameters.add(self)
+
+
+class ChoiceParameter(AddParameterWithWarningMixIn):
     # TODO: Documentation
     def __init__(self, parameter_name, choices):
         # TODO: Documentation
@@ -50,11 +67,17 @@ class ChoiceParameter(ParameterDecorator):
             choices = [choices]
         self.__choices = choices
 
-    def execute(self, parameters):
-        parameters[self.parameter_name] = self.__choices
+    @property
+    def choices(self):
+        return self.__choices
 
 
-class NormalParameter(ParameterDecorator):
+class BooleanParameter(ChoiceParameter):
+    def __init__(self, parameter_name):
+        super(BooleanParameter, self).__init__(parameter_name, [True, False])
+
+
+class NormalParameter(AddParameterWithWarningMixIn):
     # TODO: Documentation
     def __init__(self, parameter_name, mu, sigma):
         # TODO: Documentation
@@ -62,15 +85,16 @@ class NormalParameter(ParameterDecorator):
         self.__mu = mu
         self.__sigma = sigma
 
-    def execute(self, parameters):
-        parameters[self.parameter_name] = {
-            "type": "float",
-            "mu": self.__mu,
-            "sigma": self.__sigma
-        }
+    @property
+    def mu(self):
+        return self.__mu
+
+    @property
+    def sigma(self):
+        return self.__sigma
 
 
-class UniformParameter(ParameterDecorator):
+class UniformParameter(AddParameterWithWarningMixIn):
     # TODO: Documentation
     def __init__(self, parameter_name, min_value, max_value):
         # TODO: Documentation
@@ -78,38 +102,39 @@ class UniformParameter(ParameterDecorator):
         self.__min = min_value
         self.__max = max_value
 
-    def execute(self, parameters):
-        parameters[self.parameter_name] = {
-            "type": "float",
-            "min": self.__min,
-            "max": self.__max
-        }
+    @property
+    def min(self):
+        return self.__min
+
+    @property
+    def max(self):
+        return self.__max
 
 
-class QNormalParameter(NormalParameter):
+class QMixin(object):
+    def __init__(self, q):
+        self.__q = q
+
+    @property
+    def q(self):
+        return self.__q
+
+class QNormalParameter(NormalParameter, QMixin):
     # TODO: Documentation
     def __init__(self, parameter_name, mu, sigma, q):
         super(QNormalParameter, self).__init__(parameter_name=parameter_name, mu=mu, sigma=sigma)
-        self.__q = q
-
-    def execute(self, parameters):
-        super(QNormalParameter, self).execute(parameters)
-        parameters[self.parameter_name]["q"] = self.__q
+        QMixin.__init__(self, q=q)
 
 
-class QUniformParameter(NormalParameter):
+class QUniformParameter(UniformParameter, QMixin):
     # TODO: Documentation
     def __init__(self, parameter_name, mu, sigma, q):
         super(QUniformParameter, self).__init__(parameter_name=parameter_name, mu=mu, sigma=sigma)
-        self.__q = q
-
-    def execute(self, parameters):
-        super(QUniformParameter, self).execute(parameters)
-        parameters[self.parameter_name]["q"] = self.__q
+        QMixin.__init__(self, q=q)
 
 
 class NoOptimizationParameter(ParameterDecorator):
     # TODO: Documentation
     def execute(self, parameters):
-        if self.parameter_name in parameters.keys():
-            del parameters[self.parameter_name]
+        if self in parameters:
+            parameters.remove(self)
